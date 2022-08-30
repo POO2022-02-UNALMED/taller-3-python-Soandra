279abf61edf7be9ecd23d5a60fb132b855689d34
Sofia Andrade <soandra8@gmail.com>
2022-08-29 19:45:32 -0500

    Intento n

--- televisores/tv.py
+++ televisores/tv.py
@@ -72,8 +72,8 @@ class TV:
     
     def volumenUp(self):
         if self._estado and self._volumen >= 0 and self._volumen < 7:
-            self._canal += 1
+            self._volumen += 1
 
     def volumenDown(self):
         if self._estado and self._volumen > 0 and self._volumen <= 7:
-            self._canal -= 1
+            self._volumen -= 1
